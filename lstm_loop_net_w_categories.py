3dcb4a139f2b27b339030895ee8a6313202443b4
zaragozamartin91 <zaragozamartin91@gmail.com>
2018-05-16 17:24:32 -0300

    se comenzo a trabajar en una version lstm con categorias y valores de prediccion mas precisos

--- lstm_loop_net_w_categories.py
+++ lstm_loop_net_w_categories.py
@@ -2,10 +2,14 @@ import numpy
 import matplotlib.pyplot as plt
 import pandas
 import math
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras import optimizers
+from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Dropout
 
 from datetime import date
 from datetime import datetime
@@ -16,104 +20,25 @@ from matplotlib.dates import num2date
 from fractions import gcd
 
 from skmatrix import categorizer
-
-
-def normalize_dataset(dataset):
-    """ Normaliza un dataset """
-    col_count = dataset.shape[1]
-    for col in range(col_count):
-        sub_ds = dataset[:, col]
-        max_value = float(max(sub_ds))
-        min_value = float(min(sub_ds))
-        diff = max_value - min_value
-        n_ds = sub_ds - min_value
-        n_ds = n_ds / diff
-        dataset[:, col] = n_ds
-    return dataset
-
-
-def de_normalize_dataset(normalized_ds, original_ds):
-    """ 
-    Des-normaliza un dataset a partir del dataset original (completo) 
-    :param normalized_ds : Dataset normalizado a des-normalizar
-    :param original_ds : Dataset ORIGINAL (no particionado) del cual obtener los valores originales
-    :return : Dataset des-normalizado
-    """
-    col_count = original_ds.shape[1]
-    for col in range(col_count):
-        original_sub_ds = original_ds[:, col]
-        min_value = min(original_sub_ds)
-        max_value = max(original_sub_ds)
-        diff = max_value - min_value
-        a = normalized_ds[:, col] * diff
-        b = a + min_value
-        normalized_ds[:, col] = b
-    return normalized_ds
-
-
-def plot_w_xticks(all_xticks, major_xticks, major_xticks_labels, yplots):
-    """ 
-    Plotea sets de datos
-    :param all_xticks : todos los valores del eje x
-    :param major_xticks : los valores principales del eje x (valores a mostrar)
-    :param major_xticks_labels : labels de los principales valores del eje x (arreglo de strings)
-    :param yplots : arreglo de tuplas '(datos_eje_y , color_ploteo)'
-    """
-    fig = plt.figure()
-    graph = fig.add_subplot(111)
-    x = all_xticks
-    for yplot in yplots:
-        graph.plot(x, yplot[0], yplot[1])
-    graph.set_xticks(major_xticks)
-    graph.set_xticklabels(major_xticks_labels)
-    return graph
-
-
-def append_prev_demand(vars_ds, demand_ds):
-    """ Crea un nuevo dataset que contenga los valores de las variables de entrada y una columna extra que representa la demanda neta del dia anterior """
-    in_demands = [(0, )]
-    row_count = len(vars_ds)
-    for i in range(row_count):
-        if (i == 0): continue
-        prev_entry = demand_ds[i - 1]
-        in_demands.append((prev_entry[0], ))
-    return numpy.hstack((vars_ds, numpy.array(in_demands)))
-
-
-def build_win_matrix(dataset, win_size):
-    """ 
-    Crea una matriz tridimensional en la que cada entrada contiene los datos de ese dia y los datos de los win_size dias anteriores  
-    """
-    win_matrix = []
-    row_count = len(dataset)
-    for idx in range(win_size, row_count):
-        lower_limit = idx - win_size
-        entries = dataset[lower_limit:idx + 1]
-        row = []
-        for entry in entries:
-            row.append(entry)
-        win_matrix.append(row)
-    return numpy.array(win_matrix)
+from skmatrix import noise_remover
 
 
 numpy.random.seed(7)
 
-input_file = 'full_entrada_salida_pesos_506.csv'
+input_file = 'full_entrada_salida_pesos_151.csv'
 
 # COLUMNAS:
-#  0     1   2    3     4         5      6         7          8      9
-# index,dow,dom,month,year,in_demand,out_demand,prev_holi,pos_holi,minfl
-vars_df = pandas.read_csv(input_file, usecols=[1, 2, 3, 7, 8])
+#  0     1   2    3     4         5      6         7     
+# index,dow,dom,month,year,in_demand,out_demand,holi_flag
 demand_df = pandas.read_csv(input_file, usecols=[5, 6])
 
 DIFF_DEMAND = demand_df.values[:,0] - demand_df.values[:,1]
 DIFF_DEMAND.resize( (len(DIFF_DEMAND) , 1) )
 
-#  0     1   2    3     4         5      6         7          8      9
-# index,dow,dom,month,year,in_demand,out_demand,prev_holi,pos_holi,minfl
+#  0     1   2    3     4         5      6         7     
+# index,dow,dom,month,year,in_demand,out_demand,holi_flag
 dates_ds = pandas.read_csv(input_file, usecols=[2, 3, 4]).values
 
-vars_ds = vars_df.values.astype('float64')
 demand_ds = demand_df.values.astype('float64')
 
 in_demand = demand_ds[:, 0]
@@ -121,82 +46,75 @@ out_demand = demand_ds[:, 1]
 demand_ds = in_demand - out_demand
 demand_ds.resize((len(demand_ds), 1))
 
+# elimino las entradas con ruido de la primera region
+a = noise_remover.remove_max_on_region(demand_ds , 4 , 0 , 300)
+b = noise_remover.remove_min_on_region(a , 5 , 0 , 300)
+demand_ds = b.copy()
+
+
 # obtengo los valores de demanda categorizados
-CAT_COUNT = 30
-categorized_demand, demand_bags = categorizer.categorize_real_w_equal_frames(demand_ds, cat_count=CAT_COUNT)
+CAT_COUNT = 40
+categorized_demand, demand_bags = categorizer.categorize_real_w_equal_frames(demand_ds.reshape( (len(demand_ds) , 1) ), cat_count=CAT_COUNT)
 demand_ds = categorized_demand.copy()
 
-# Agrego los valores de la demanda de entrada y salida previas
-vars_ds = append_prev_demand(vars_ds, demand_ds)
-# Descarto el primer dia dado que no cuenta con demanda previa o su demanda previa es un falso 0.0
-vars_ds = vars_ds[1:]
-demand_ds = demand_ds[1:]
-dates_ds = dates_ds[1:]
+# normalizo
+demand_ds = demand_ds / demand_ds.max()
 
-# Asigno valores de 0 a 1 a todas las entradas
-normalize_dataset(vars_ds)
-normalize_dataset(demand_ds)
+ds_size = len(demand_ds) # tamano del dataset
 
-column_count = vars_ds.shape[1]  # Cantidad de columnas del dataset de entrada
-timesteps = 10  # cantidad de pasos memoria
-vars_ds = build_win_matrix(vars_ds, timesteps)
-# hago coincidir los dias y las demandas con la nueva matriz de ventana
-demand_ds = demand_ds[timesteps:]
-dates_ds = dates_ds[timesteps:]
+# prepare the dataset of input to output pairs encoded as integers
+seq_length = 100
+dataX = []
+dataY = []
+for i in range(0, ds_size - seq_length, 1):
+    seq_in = demand_ds.flatten()[i:i + seq_length]
+    seq_out = demand_ds.flatten()[i + seq_length]
+    dataX.append(seq_in)
+    dataY.append(seq_out)
 
-# reshape hacia (CANT_FILAS, CANT_ESTADOS = 1 , CANT_COLUMNAS)
-# este reshape se hace para trabajar con LSTM con timesteps == 1
-# vars_ds = vars_ds.reshape((len(vars_ds), 1, column_count))
+n_patterns = len(dataX)
+print("Total Patterns: ", n_patterns)
 
-# train_size = int(len(vars_ds) * 0.67)
-# test_size = len(vars_ds) - train_size
+# reshape X to be [samples, time steps, features]
+X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+# one hot encode the output variable
+y = np_utils.to_categorical(dataY)
 
+batch_size = 5
 test_size = 30
-train_size = len(vars_ds) - test_size
-# train_size = 675
+train_size = batch_size * int((len(X) - test_size) / batch_size)
 
 train_lower_limit = 0
 train_upper_limit = train_size
 test_lower_limit = train_size
 test_upper_limit = train_size + test_size
 
-# Creo el dataset de entrenamiento. EL mismo toma las filas
-# del 0 a train_size (no inclusive) y todas las columnas (rango ':')
-train_vars = vars_ds[train_lower_limit:train_upper_limit]
-train_demand = demand_ds[train_lower_limit:train_upper_limit]
 
-test_vars = vars_ds[test_lower_limit:test_upper_limit]
-test_demand = demand_ds[test_lower_limit:test_upper_limit]
+train_x = X[train_lower_limit:train_upper_limit]
+train_y = y[train_lower_limit:train_upper_limit]
+test_x = X[test_lower_limit:test_upper_limit]
+test_y = y[test_lower_limit:test_upper_limit]
 true_dates = dates_ds[test_lower_limit:test_upper_limit]  # obtengo las fechas a graficar
 
-# Keras de manera implicita trabaja siempre con una capa de entrada
-# input_dim determina la cantidad de neuronas de la capa de entrada
-# el primer parametro de 'Dense' es la cantidad de neuronas de la capa oculta
-# una red densa o Dense es aquella en la que todas las neuronas de una capa N estan conectadas con todas las de la capa N+1
-# el modelo a crear es Multilayer Perceptron
-# la cantidad de neuronas de la capa de salida debe coincidir con la cantidad de valores a predecir
-# Si la ultima capa tiene una funcion de activacion, entonces estamos modelando un problema de CLASIFICACION / CLUSTERIZACION en vez de uno de PREDICCION
-# epochs es el número de pasadas por todo el conjunto de datos de entrenamiento
-# batch_size es el número de muestras que se usan para calcular una actualización de los pesos
-# LA CAPA N+1 NO DEBERIA SER MAS GRANDE QUE LA CAPA N
 
-# input_dim = train_vars.shape[1] # la cantidad de neuronas de input es igual a la cantidad de columnas del dataset de entrada
+
+
+# define the LSTM model
 model = Sequential()
+model.add(LSTM(256 , input_shape=(train_x.shape[1], train_x.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(train_y.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+# define the checkpoint
+filepath = "weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+# fit the model
+model.fit(train_x, train_y, epochs=20, batch_size=batch_size, callbacks=callbacks_list , shuffle=False)
 
-# keras.layers.LSTM(units, activation='tanh', recurrent_activation='hard_sigmoid', use_bias=True, kernel_initializer='glorot_uniform', recurrent_initializer='orthogonal', bias_initializer='zeros', unit_forget_bias=True, kernel_regularizer=None, recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, recurrent_constraint=None, bias_constraint=None, dropout=0.0, recurrent_dropout=0.0, implementation=1, return_sequences=False, return_state=False, go_backwards=False, stateful=False, unroll=False)
 
-# batch_size = 5
-# batch_size = gcd(train_size , test_size)
-batch_size = 1
-epochs = 200
-model.add(LSTM(30, stateful=True, batch_input_shape=(batch_size, timesteps + 1, column_count)))
-# model.add(LSTM(50, input_shape=(1,vars_ds.shape[2]) , stateful=True, batch_input_shape=(batch_size,1,vars_ds.shape[2])) )
-model.add(Dense(30, activation='relu'))
-model.add(Dense(1, activation='relu'))
-opt = optimizers.adam(lr=0.001)
-model.compile(loss='binary_crossentropy', optimizer=opt)
-# model.compile(loss='mean_squared_error', optimizer=opt)
-model.fit(train_vars, train_demand, epochs=epochs, batch_size=batch_size, shuffle=False, verbose=2)
+
+
 
 idx = 0
 predicted = []
--- skmatrix/noise_remover.py
+++ skmatrix/noise_remover.py
@@ -4,11 +4,20 @@ import numpy as np
 def remove_max(ds, count):
     if count is 0: return ds
     maxv = ds.argmax()
-    return remove_max(np.delete(ds, maxv), count - 1)
+    return remove_max(np.delete(ds, maxv, axis=0), count - 1)
 
 
 def remove_min(ds, count):
     if count is 0: return ds
     minv = ds.argmin()
-    return remove_min(np.delete(ds, minv), count - 1)
+    return remove_min(np.delete(ds, minv, axis=0), count - 1)
 
+
+def remove_max_on_region(ds, count, start_idx, end_idx):
+    a = remove_max(ds[start_idx:end_idx], count)
+    return np.vstack([ds[0:start_idx], a, ds[end_idx:]])
+
+
+def remove_min_on_region(ds, count, start_idx, end_idx):
+    a = remove_min(ds[start_idx:end_idx], count)
+    return np.vstack([ds[0:start_idx], a, ds[end_idx:]])
